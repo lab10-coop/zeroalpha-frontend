fcd1ad62fa08647f72f129a071a066035ff37cd0
PeterTheOne <petertheone@gmail.com>
2020-11-25 19:25:57 +0100

    add bnc-onboard to connect wallets..

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import Onboard from 'bnc-onboard';
+import { Button } from 'reactstrap';
+import { toChecksumAddress } from 'web3-utils';
 import stewardContract from './contract-instances/steward';
 import nftContract, { id as tokenId } from './contract-instances/nft';
 
+const networkId = parseInt(process.env.REACT_APP_CHAIN_N_ID || '', 10);
+const rpc = process.env.REACT_APP_CHAIN_RPC || '';
+
 export default function App(): JSX.Element {
+  const onboard = Onboard({
+    networkId,
+    walletSelect: {
+      wallets: [
+        { walletName: 'metamask', preferred: true },
+        {
+          walletName: 'walletConnect',
+          rpc: {
+            [networkId]: rpc,
+          },
+          preferred: true,
+        },
+      ],
+    },
+  });
+
+  const [address, setAddress] = useState<string | undefined>(undefined);
   const [name, setName] = useState<string>('');
   const [symbol, setSymbol] = useState<string>('');
   const [imageUrl, setImageUrl] = useState<string>('');
@@ -10,6 +33,8 @@ export default function App(): JSX.Element {
   const [artist, setArtist] = useState<string>('');
   const [price, setPrice] = useState<string>('');
   const [collected, setCollected] = useState<string>('');
+
+  const shortAddr = (addr: string): string => `${addr.substr(0, 6)}…${addr.substr(-4)}`;
 
   const fetchNftInfo = async () => {
     const tokenUri = await nftContract.methods.tokenURI(tokenId).call();
@@ -48,13 +73,30 @@ export default function App(): JSX.Element {
     fetchStewardInfo();
   }, []);
 
+  const connectWallet = async () => {
+    const selectResult = await onboard.walletSelect();
+    if (!selectResult) {
+      return;
+    }
+    await onboard.walletCheck();
+    setAddress(onboard.getState().address);
+  };
+
   return (
     <div className="container">
 
       <header>
         <h1>ZeroAlpha</h1>
 
-        <p>*connect*</p>
+        <Button color="primary" onClick={() => connectWallet()}>
+          {(!address) ? (
+            <span>Connect wallet</span>
+          ) : (
+            <span title="Switch wallet">
+              {shortAddr(toChecksumAddress(address))}
+            </span>
+          )}
+        </Button>
       </header>
 
       <img src={imageUrl} alt={name} />
